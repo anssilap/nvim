d3cb403cf6946abb54899a89469e2c2e7fee0ff7
Anssi Lappalainen <anssilap@users.noreply.github.com>
2023-02-14 20:53:52 +0200

    Add autowrap for markdown and text

--- init.lua
+++ init.lua
@@ -9,3 +9,10 @@ require("settings.packer")
 require("settings.set")
 require("settings.keybinds")
 
+-- Set wordwrap for given filetypes
+vim.api.nvim_command("augroup filetype_wrap")
+vim.api.nvim_command("autocmd!")
+vim.api.nvim_command("autocmd FileType markdown setlocal wrap")
+vim.api.nvim_command("autocmd FileType text setlocal wrap")
+vim.api.nvim_command("augroup END")
+
